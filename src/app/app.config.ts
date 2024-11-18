3e862dea94737994d37fffa10025b60cb69f04e5
Bruno Kaiser <bruno@bkaiser.ch>
2024-11-18 20:08:05 +0100

    add project config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       provideStorage(() => getStorage()),
       provideMessaging(() => getMessaging())
     ),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-439b4","appId":"1:986698560145:web:435a90a86b694af64c4530","storageBucket":"friendlychat-439b4.firebasestorage.app","apiKey":"AIzaSyDZEMSEoEFip4sBjDqQL--GIC_FvqCNUno","authDomain":"friendlychat-439b4.firebaseapp.com","messagingSenderId":"986698560145","measurementId":"G-P6EMNXYXB9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+    apiKey: "AIzaSyDZEMSEoEFip4sBjDqQL--GIC_FvqCNUno",
+    databaseURL: 'https://friendlychat-439b4.firebaseio.com',
+    authDomain: "friendlychat-439b4.firebaseapp.com",
+    projectId: "friendlychat-439b4",
+    storageBucket: "friendlychat-439b4.firebasestorage.app",
+    messagingSenderId: "986698560145",
+    appId: "1:986698560145:web:435a90a86b694af64c4530",
+    measurementId: "G-P6EMNXYXB9"
+  }
 };
