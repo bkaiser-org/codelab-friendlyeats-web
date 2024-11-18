56219f7625cef876b65ad1707ad615fb3315daca
Bruno Kaiser <bruno@bkaiser.ch>
2024-11-18 21:11:38 +0100

    removing redundant providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
 import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
@@ -11,14 +10,7 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-439b4","appId":"1:986698560145:web:435a90a86b694af64c4530","storageBucket":"friendlychat-439b4.firebasestorage.app","apiKey":"AIzaSyDZEMSEoEFip4sBjDqQL--GIC_FvqCNUno","authDomain":"friendlychat-439b4.firebaseapp.com","messagingSenderId":"986698560145","measurementId":"G-P6EMNXYXB9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-439b4","appId":"1:986698560145:web:435a90a86b694af64c4530","storageBucket":"friendlychat-439b4.firebasestorage.app","apiKey":"AIzaSyDZEMSEoEFip4sBjDqQL--GIC_FvqCNUno","authDomain":"friendlychat-439b4.firebaseapp.com","messagingSenderId":"986698560145","measurementId":"G-P6EMNXYXB9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
